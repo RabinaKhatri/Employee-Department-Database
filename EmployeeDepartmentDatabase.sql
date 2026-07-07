271603c4641058f3e40af8f4379ed8b983eb5a7b
Rabina Khatri <rabinabhattarai74@gmail.com>
2026-07-07 12:17:58 +0545

    Add Age and JoiningDate columns to Employees table

--- EmployeeDepartmentDatabase.sql
+++ EmployeeDepartmentDatabase.sql
@@ -44,3 +44,205 @@ CREATE TABLE Employees(
  (115, 'Rohan','','Basnet',59000,03,1);
  
  SELECT * FROM Employees;
+
+--To see the entire table of both Employees and Department
+Select * From Employees;
+Select * From Department;
+
+--Display only the FirstName Column.
+Select FirstName From Employees;
+
+--Display only the Salary Column.
+Select Salary From Employees;
+
+--Display FirstName, LastName, and Email
+Select FirstName, LastName From Employees;
+
+--Display Salary As MonthlySalary
+Select Salary As MonthlySalary From Employees;
+
+--Display DepName As DepartmentName
+Select DepName As DepartmentName From Department;
+
+--Display all unique departments from the Employees table
+Select Distinct(DepName) From Department;
+
+--Display employees sorted by salary in descending order
+Select * From Employees
+Order By Salary Desc;
+
+--Display employees sorted alphabetically by LastName in ascending order
+Select * From Employees
+Order By LastName;
+
+--Display employees sorted by isActive
+Select * From Employees
+Order By isActive;
+
+--Display employees sorted first by Department and then by Salary
+Select * From Employees
+Order By Depid, Salary;
+
+--Display the first 5 employees
+Select Top 5 * From Employees;
+
+--Display the top 7 highest-paid employees
+Select Top 7 * From Employees
+Order By Salary;
+
+--Display employee names in alphabetical order
+Select FirstName, MiddleName, LastName From Employees
+Order By FirstName, MiddleName, LastName;
+
+--Display employees IDs in descending order
+Select Empid From Employees
+Order By Empid Desc;
+
+--Adding the Age Column in Employees Table
+ALTER TABLE Employees
+ADD Age INT;
+SELECT * FROM Employees;
+
+--Adding values in Age column of each employee
+UPDATE Employees
+SET Age =25
+WHERE Empid = 101;
+
+UPDATE Employees
+SET Age =21
+WHERE Empid = 102;
+
+UPDATE Employees
+SET Age =28
+WHERE Empid = 103;
+
+UPDATE Employees
+SET Age =29
+WHERE Empid = 104;
+
+UPDATE Employees
+SET Age = 24
+WHERE Empid = 105;
+
+UPDATE Employees
+SET Age = 23
+WHERE Empid = 106;
+
+UPDATE Employees
+SET Age = 33
+WHERE Empid = 107;
+
+UPDATE Employees
+SET Age = 34
+WHERE Empid = 108;
+
+UPDATE Employees
+SET Age = 35
+WHERE Empid = 109;
+
+UPDATE Employees
+SET Age = 41
+WHERE Empid = 110;
+
+UPDATE Employees
+SET Age = 33
+WHERE Empid = 111;
+
+UPDATE Employees
+SET Age = 34
+WHERE Empid = 112;
+
+UPDATE Employees
+SET Age = 23
+WHERE Empid = 113;
+
+UPDATE Employees
+SET Age = 43
+WHERE Empid = 114;
+
+UPDATE Employees
+SET Age = 47	
+WHERE Empid = 115;
+
+--Write one query that shows all columns but sorts employees by age
+Select * From Employees
+Order By Age;
+
+--Display all unique ages
+Select Distinct(Age) From Employees;
+
+
+--Adding JoiningDate Column in Employees Table
+ALTER TABLE Employees
+ADD JoiningDate DATE;
+
+--Adding values in JoiningDate column of each employee
+UPDATE Employees
+SET JoiningDate = '2023-01-15'
+WHERE Empid = 101;
+
+UPDATE Employees
+SET JoiningDate = '2023-02-10'
+WHERE Empid = 102;
+
+UPDATE Employees
+SET JoiningDate = '2024-05-15'
+WHERE Empid = 103;
+
+UPDATE Employees
+SET JoiningDate = '2025-06-15'
+WHERE Empid = 104;
+
+UPDATE Employees
+SET JoiningDate = '2025-07-20'
+WHERE Empid = 105;
+
+UPDATE Employees
+SET JoiningDate = '2022-08-25'
+WHERE Empid = 106;
+
+UPDATE Employees
+SET JoiningDate = '2021-09-01'
+WHERE Empid = 107;
+
+UPDATE Employees
+SET JoiningDate = '2020-01-01'
+WHERE Empid = 108;
+
+UPDATE Employees
+SET JoiningDate = '2022-03-02'
+WHERE Empid = 109;
+
+UPDATE Employees
+SET JoiningDate = '2021-02-19'
+WHERE Empid = 110;
+
+UPDATE Employees
+SET JoiningDate = '2021-10-10'
+WHERE Empid = 111;
+
+UPDATE Employees
+SET JoiningDate = '2023-10-20'
+WHERE Empid = 112;
+
+UPDATE Employees
+SET JoiningDate = '2024-06-17'
+WHERE Empid = 113;
+
+UPDATE Employees
+SET JoiningDate = '2025-07-07'
+WHERE Empid = 114;
+
+UPDATE Employees
+SET JoiningDate = '2022-05-20'
+WHERE Empid = 115;
+
+--Display employees sorted by JoiningDate
+Select FirstName, MiddleName, LastName, JoiningDate From Employees
+Order By JoiningDate;
+
+--Display all unique JoiningDate values
+Select Distinct(JoiningDate) From Employees;
+
+Select * From Employees;
+
